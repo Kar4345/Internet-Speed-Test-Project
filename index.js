525a4f36bc60773ab26dea2e012a6f4f582e2b58
Kar4345 <103517760+Kar4345@users.noreply.github.com>
2022-09-07 23:33:25 +0530

    updation

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let speedtest = new FastSpeedtest({
   token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm", // required
   verbose: false, // default: false
   timeout: 5000, // default: 5000
-  https: true, // default: true
+  https: false, // default: true
   urlCount: 5, // default: 5
   bufferSize: 8, // default: 8
   unit: FastSpeedtest.UNITS.Mbps, // default: Bps
